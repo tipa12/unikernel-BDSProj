fed3c1ee227478ff7bed16032441099d88f32437
Tilmann <t.pabst@campus.tu-berlin.de>
2023-01-05 17:24:07 +0100

    Refactoring
    removed duplicate code, added evaluations datastore

--- dataService/main.py
+++ dataService/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from tupleGenerator import generate_tuples
 from google.cloud import firestore
 # Imports the Google Cloud client library
@@ -13,7 +13,7 @@ app = Flask(__name__)
 def root():
     return 'Please give an argument to the URL.'
 
-def generateUniqueID():
+def generateUniqueDatasetId():
     # Generate a unique ID
     uniqueId = str(uuid.uuid4())
 
@@ -24,14 +24,29 @@ def generateUniqueID():
     dateTimeStr = now.strftime("%Y%m%d%H%M%S")
 
     # Concatenate the unique ID and the date/time string
-    uniqueIdWithDateTime = dateTimeStr + "-" + uniqueId
+    uniqueIdWithDateTime = 'ds-' + dateTimeStr + '-' + uniqueId
+
+    return uniqueIdWithDateTime
+
+def generateUniqueEvaluationId():
+    # Generate a unique ID
+    uniqueId = str(uuid.uuid4())
+
+    # Get the current date and time
+    now = datetime.datetime.now()
+
+    # Format the date and time as a string
+    dateTimeStr = now.strftime("%Y%m%d%H%M%S")
+
+    # Concatenate the unique ID and the date/time string
+    uniqueIdWithDateTime = 'ev-' + dateTimeStr + '-' + uniqueId
 
     return uniqueIdWithDateTime
 
 def tupleToDict(tuple):
     return {str(i): val for i, val in enumerate(tuple)}
 
-def uploadObjectToCloudStorage(objectToUpload, bucketName, uniqueFilename):
+def uploadObjectToCloudStorage(uniqueFilename, objectToUpload, bucketName):
     # Writing to tmp folder
     with open("/tmp/" + uniqueFilename + ".pkl", "wb") as file:
         # Serialize the list
@@ -46,35 +61,26 @@ def uploadObjectToCloudStorage(objectToUpload, bucketName, uniqueFilename):
     blob = bucket.blob(uniqueFilename + ".pkl")
     blob.upload_from_filename("/tmp/" + uniqueFilename + ".pkl")
 
-
-def writeDatasetToFirestoreAndCloudStorage(name, sizeOfTuples, numberOfTuples, elementType, elementRangeStart, elementRangeEnd, listOfTuples, unikernelFunction):
-
-    datasetId = generateUniqueID()
-    # Define the bucket and object name
-    bucketName = 'datasetbucket3245'
-
+def updateFirestore(collection, documentId, updateDict):
     # Create a Firestore client
     db = firestore.Client()
 
     # Create a new document in the "datasets" collection
-    doc_ref = db.collection("datasets").document(datasetId)
+    docRef = db.collection(collection).document(documentId)
+    updateDict.update({'lastmodified': datetime.datetime.now()})
 
     # Set the data for the document
-    doc_ref.set({
-        "name": name,
-        "sizeOfTuples": sizeOfTuples,
-        "numberOfTuples": numberOfTuples,
-        "elementType": elementType,
-        "elementRangeStart": elementRangeStart,
-        "elementRangeEnd": elementRangeEnd,
-        "cloudStorageBucket": bucketName,
-        "datasetFilename": datasetId + ".pkl",
-        "datasetId": datasetId,
-        "dateCreated": datetime.datetime.now(),
-        "unikernelFunction": unikernelFunction
-    })
-    uploadObjectToCloudStorage(listOfTuples, bucketName, datasetId)
-    return datasetId
+    docRef.update(updateDict)
+
+def createFirestoreDocument(collection, documentId, setDict):
+
+    # Create a Firestore client
+    db = firestore.Client()
+    # Create a new document in the "datasets" collection
+    docRef = db.collection(collection).document(documentId)
+    setDict.update({"created": datetime.datetime.now(), 'lastmodified': datetime.datetime.now()})
+    # Set the data for the document
+    docRef.set(setDict)
 
 def downloadDataset(datasetId):
     # Set up the Cloud Storage client
@@ -94,6 +100,19 @@ def downloadDataset(datasetId):
 
     return data
 
+def generateDataset(datasetId, sizeOfTuples, numberOfTuples, elementType, elementRangeStart, elementRangeEnd):
+
+    # generate tuples
+    listOfTuples = generate_tuples(sizeOfTuples, numberOfTuples, elementType, (elementRangeStart, elementRangeEnd))
+
+    # bucket name for cloud storage
+    bucketName = 'datasetbucket3245'
+
+    uploadObjectToCloudStorage(datasetId, listOfTuples, bucketName)
+    updateFirestore('datasets', datasetId, {'uploadedToCloudStorage': True, "datasetFilename": datasetId + ".pkl"})
+
+    return datasetId
+
 def evaluateDataset(datasetId, unikernelFunction):
     data = downloadDataset(datasetId)
     acceptedTuples = 0
@@ -105,44 +124,90 @@ def evaluateDataset(datasetId, unikernelFunction):
                 acceptedTuples += 1
             else:
                 rejectedTuples += 1
+    elif unikernelFunction == 'map':
+        acceptedTuples = len(data)
+    else:
+        print("Error: Unikernel function not recognized")
 
-    # Create a Firestore client
-    db = firestore.Client()
-
-    # Create a new document in the "datasets" collection
-    doc_ref = db.collection("datasets").document(datasetId)
-
-    # Set the capital field
-    doc_ref.update({
-        'acceptedTuples': acceptedTuples,
-        'rejectedTuples': rejectedTuples
-        })
+    return acceptedTuples, rejectedTuples
 
 
 @app.route('/generateDataset')
 def generateDatasetEndpoint():
+    allParams = request.args.to_dict()
+
     # set parameters/arguments
     sizeOfTuples = int(request.args.get('sizeOfTuples'))
     numberOfTuples = int(request.args.get('numberOfTuples'))
     elementType = request.args.get('elementType')
     elementRangeStart = int(request.args.get('elementRangeStart'))
     elementRangeEnd = int(request.args.get('elementRangeEnd'))
-    unikernelFunction = request.args.get('unikernelFunction')
+    name = request.args.get('name')
 
-    # generate tuples
-    listOfTuples = generate_tuples(sizeOfTuples, numberOfTuples, elementType, (elementRangeStart, elementRangeEnd))
-    datasetId = writeDatasetToFirestoreAndCloudStorage("default", sizeOfTuples, numberOfTuples, elementType, elementRangeStart, elementRangeEnd, listOfTuples, unikernelFunction)
+    # generate datasetId
+    datasetId = generateUniqueDatasetId()
+    if not request.args.get('name'):
+        name = 'default'
 
-    #return tuples
-    return datasetId
+    firestoreDict = {
+        "name": name,
+        "sizeOfTuples": sizeOfTuples,
+        "numberOfTuples": numberOfTuples,
+        "elementType": elementType,
+        "elementRangeStart": elementRangeStart,
+        "elementRangeEnd": elementRangeEnd,
+        "datasetId": datasetId
+    }
+
+    createFirestoreDocument('datasets', datasetId, firestoreDict)
+
+    # generate dataset
+    generateDataset(datasetId, sizeOfTuples, numberOfTuples, elementType, elementRangeStart, elementRangeEnd)
+
+    #return datasetId, parameters
+    response = {
+        'datasetId': datasetId,
+        'message': 'Success',
+        'parameters': allParams
+    }
+    return jsonify(response)
 
 
 @app.route('/evaluateDataset')
 def evaluateDatasetEndpoint():
+    allParams = request.args.to_dict()
+    # function to evaluate dataset: filter, map
+    unikernelFunction = request.args.get('unikernelFunction')
     datasetId = request.args.get('datasetId')
-    evaluateDataset(datasetId, 'filter')
-    return datasetId
-    
+
+    evaluationId = generateUniqueEvaluationId()
+
+    firestoreDict = {
+        "unikernelFunction": unikernelFunction,
+        "datasetId": datasetId,
+        "evaluationId": evaluationId
+    }
+
+    createFirestoreDocument('evaluations', evaluationId, firestoreDict)
+
+    acceptedTuples, rejectedTuples = evaluateDataset(datasetId, unikernelFunction)
+
+    updateDict = {
+        "acceptedTuples": acceptedTuples,
+        "rejectedTuples": rejectedTuples
+    }
+
+    updateFirestore('evaluations', evaluationId, updateDict)
+
+    #return datasetId, evaluationId, parameters
+    response = {
+        'datasetId': datasetId,
+        'evaluationId': evaluationId,
+        'message': 'Success',
+        'parameters': allParams
+    }
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
