51f8c6d6db7d06ff0fe7d7cae2812882a991eac5
Tilmann <t.pabst@campus.tu-berlin.de>
2023-01-15 11:50:41 +0100

    update startup script

--- dataService/start.sh
+++ dataService/start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Update package manager and install packages
-sudo apt update -y
-sudo apt install -y python3-pip
-pip3 install -r requirements.txt
-gunicorn -w 4 -b 0.0.0.0:8000 main:app
+# Start the data service
+nohup python3 -m main.py &
