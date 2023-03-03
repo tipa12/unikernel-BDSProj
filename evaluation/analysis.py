0c766b8381bc9daf7b7cd86b75ca3bc302c9d5c5
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2023-03-03 12:38:22 +0100

    Evaluation: final submission

--- evaluation/analysis.py
+++ evaluation/analysis.py
@@ -12,4 +12,12 @@ def get_recv_latency(experiment: Experiment):
 
 
 def get_number_of_dropped_tuples(experiment: Experiment):
-    return experiment.sink_data.number_of_tuples_recv - experiment.source_data.number_of_tuples_passing_the_filter
+    return experiment.sink_data['measurements'][0]['number_of_tuples_recv'] - experiment.source_data.number_of_tuples_passing_the_filter
+
+def p99_latency(values: [float]):
+    latencies_sorted = sorted(values)  # sort the latencies
+    index_p99 = int(len(values) * 0.99)
+    if len(latencies_sorted) <= index_p99 + 1:
+        return (latencies_sorted[-1] + latencies_sorted[-2]) / 2
+
+    return (latencies_sorted[index_p99] + latencies_sorted[index_p99 + 1]) / 2
